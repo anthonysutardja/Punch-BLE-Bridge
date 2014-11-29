c70db372e5b12354a77eb118f1ccf0836076b11d
anthonysutardja <anthony.sutardja@gmail.com>
2014-11-29 01:11:56 -0800

    Added asyncblocking for fetching characteristic values.

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ var request = require('request');
 var noble = require('noble');
 var uuid = require('node-uuid');
 var fs = require('fs');
+var asyncblock = require('asyncblock');
 
 
 /* Settings */
 var PUNCH_UUID_FILENAME = '.punch_uuid';
 var PUNCH_HOSTNAME = 'http://localhost:8000';
 
-var PUNCH_BLE_SERVICE_UUID = 'b0bb58205a0d11e493ee0002a5d5c51b';
-var PUNCH_BLE_TEMP_CHAR_UUID = '';
+var PUNCH_BLE_SERVICE_UUID =   'b0bb58205a0d11e493ee0002a5d5c51b';
+var PUNCH_BLE_TEMP_CHAR_UUID = '7a77be205a0d11e4a95e0002a5d5c51b';
 var PUNCH_BLE_BRIX_CHAR_UUID = '';
 
 
@@ -75,30 +76,60 @@ PunchBridge.createSensorData = function(sensor_uuid, temperature, brix) {
 /**
  * Return a float represented in hex.
  * 
- * @param  {string} hex_string A hex string storing a float in left endian format.
- * @return {number}            The number representation
+ * @param  {Buffer} buffer A hex string storing a float in left endian format.
+ * @return {number}        The number representation
  */
-PunchBridge.convertHexToFloat = function(hex_string) {
-    return Buffer(hex_string, 'hex').readFloatLE(0);
+PunchBridge.convertHexToFloat = function(buffer) {
+    return buffer.readFloatLE(0);
 };
 
 
 /* BLE setup */
 noble.on('stateChange', function(state){
     if (state === 'poweredOn') {
+        // Parameters: Punch BLE service UUIDs and if we allow duplicates
+        //noble.startScanning([PUNCH_BLE_SERVICE_UUID], true);
         noble.startScanning();
     } else {
         noble.stopScanning();
     }
 });
 
+// https://github.com/sandeepmistry/noble/wiki/Getting-started
 noble.on('discover', function(peripheral) {
+    peripheral.connect(function(error) {
+        console.log('  connected to peripheral: ' + peripheral.uuid);
+        // Get the service containing the brix and temperature characteristics
+        peripheral.discoverServices([PUNCH_BLE_SERVICE_UUID], function(error, services) {
+            if (services.length > 0) {
+                var punchService = services[0];
+                // Find each characteristic
+                // Make characteristic discoveries synchronous
+                asyncblock(function(flow) {
+                    var tempContents, tempReading, brixReading, data;
+                    // Get the temperature reading
+                    punchService.discoverCharacteristics([PUNCH_BLE_TEMP_CHAR_UUID], flow.set('tempContents'));
+                    tempContents = flow.get('tempContents');
+                    tempContents[0].read(flow.set('tempReading'));
+                    tempReading = flow.get('tempReading').readFloatLE(0);
+                    console.log("READING: " + tempReading);
+
+                    // TODO: add BRIX measurement
+                    brixReading = 10.0;
+
+                    // Push the data to the server
+                    data = PunchBridge.createSensorData(peripheral.uuid, tempReading, brixReading);
+                    console.log(data);
+                    // Disabled for now..
+                    // PunchBridge.pushData(data);
+                });
+            }
+        });
+    });
 });
 
 
 /* Main Section */
 
 PunchBridge.initalSetup();
-console.log(PunchBridge.uuid);
-// Tests
-// PunchBridge.pushData(PunchBridge.createSensorData('12345610adf', 25.0, 10.0));
+console.log("Punch Bridge UUID: " + PunchBridge.uuid);
